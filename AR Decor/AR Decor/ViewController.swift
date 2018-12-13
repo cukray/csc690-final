72a5781143c4e3cbc46ebaced573042ce94bbfd5
CUK <CUK@CUKs-MacBook-Pro.local>
2018-12-13 13:08:55 -0800

    testing

---
+++ AR Decor/AR Decor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR Decor
+//
+//  Created by CUK on 12/13/18.
+//  Copyright © 2018 U Keong Cheong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
